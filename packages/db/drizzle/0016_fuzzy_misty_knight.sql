eea36bec9ae89d86a9eecb34d9fc015c84fa34cd
mikemikimike <13286568797@163.com>
2026-08-29 20:57:30 +0800

    fix: drop obsolete notification source index

--- packages/db/drizzle/0016_fuzzy_misty_knight.sql
+++ packages/db/drizzle/0016_fuzzy_misty_knight.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX IF NOT EXISTS "notification_delivery_source_unique" ON "notification_delivery" USING btree ("source_delivery_id","user_id","channel");
+@packages/db/drizzle/0016_fuzzy_misty_knight.sql
