7c6c1aaa48eb490d3d34f17d46beeacdf1505f05
mikemikimike <13286568797@163.com>
2026-08-29 20:33:18 +0800

    fix: make notification dedupe migration idempotent

--- packages/db/drizzle/0016_fuzzy_misty_knight.sql
+++ packages/db/drizzle/0016_fuzzy_misty_knight.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX "notification_delivery_source_unique" ON "notification_delivery" USING btree ("source_delivery_id","user_id","channel");
+CREATE UNIQUE INDEX IF NOT EXISTS "notification_delivery_source_unique" ON "notification_delivery" USING btree ("source_delivery_id","user_id","channel");
